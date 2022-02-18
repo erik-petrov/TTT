47aff2c0dc6eae1ee8148c5309929f5a1698e28c
Opilane TTHK <opilane@tthk.ee>
2022-02-18 13:20:55 +0200

    Add project files.

---
+++ TTT/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TTT
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			Label lbl = new Label
+			{
+				Text = "Swipe right for bot play and left for pvp",
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center,
+				FontSize = 36,
+			};
+			SwipeGestureRecognizer swipeLeft = new SwipeGestureRecognizer();
+			swipeLeft.Direction = SwipeDirection.Left;
+			SwipeGestureRecognizer swipeRight = new SwipeGestureRecognizer();
+			swipeRight.Direction = SwipeDirection.Right;
+			swipeLeft.Swiped += SwipeLeft_Swiped;
+			swipeRight.Swiped += SwipeRight_Swiped;
+			StackLayout st = new StackLayout
+			{
+				Children = { lbl }
+			};
+			lbl.GestureRecognizers.Add(swipeRight);
+			lbl.GestureRecognizers.Add(swipeLeft);
+			Content = st;
+		}
+		async void SwipeRight_Swiped(object sender, SwipedEventArgs e)
+		{
+			await Navigation.PushAsync(new Bot());
+		}
+		async void SwipeLeft_Swiped(object sender, SwipedEventArgs e)
+		{
+			await Navigation.PushAsync(new Pvp());
+		}
+	}
+}
---
+++ TTT/Pvp.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace TTT
+{
+	public class Pvp : ContentPage
+	{
+        Image img;
+        bool x = false;
+		public Pvp()
+		{
+            Grid g = new Grid
+            {
+                RowSpacing = 0,
+                ColumnSpacing = 0,
+                RowDefinitions =
+                {
+                    new RowDefinition{Height = new GridLength(2, GridUnitType.Star) },
+                    new RowDefinition{Height = new GridLength(1, GridUnitType.Star)}
+                },
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition{Width = GridLength.Auto },
+                }
+            };
+            Grid g_ = new Grid
+            {
+                RowSpacing = 0,
+                RowDefinitions =
+                {
+                    new RowDefinition{Height = new GridLength(1, GridUnitType.Star)},
+                    new RowDefinition{Height = new GridLength(1, GridUnitType.Star)},
+                    new RowDefinition{Height = new GridLength(1, GridUnitType.Star)}
+                },
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition{Width= new GridLength(1, GridUnitType.Star)},
+                    new ColumnDefinition{Width= new GridLength(1, GridUnitType.Star)},
+                    new ColumnDefinition{Width= new GridLength(1, GridUnitType.Star)},
+                }
+            };
+            g_.BackgroundColor = Color.Red;
+            g.BackgroundColor = Color.Red;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    img = new Image();
+                    img.Source = ImageSource.FromFile("Nimetu.png");
+                    g_.Children.Add(img, i, j);
+                    var tap = new TapGestureRecognizer();
+                    img.GestureRecognizers.Add(tap);
+                    tap.Tapped += async (object sender, EventArgs e) =>
+                    {
+                        Image img = sender as Image;
+                        if (x)
+                        {
+                            img.Source = ImageSource.FromFile("krestik.png");
+                            img.GestureRecognizers.Clear();
+                            x = !x;
+                        }
+                        else
+                        {
+                            img.Source = ImageSource.FromFile("nolik.png");
+                            img.GestureRecognizers.Clear();
+                            x = !x;
+                        }
+                    };
+                }
+            }
+            g.Children.Add(g_);
+            Content = g;
+		}
+	}
+}
